3e1205b9d24786ad3e27eb6bee696bd83b23d0f3
Brandon Kuczenski <brandon.kuczenski@301south.net>
2021-06-09 13:03:32 -0700

    Add origin as config argument

--- etl/solids/sync_data.py
+++ etl/solids/sync_data.py
@@ -8,6 +8,10 @@ from dagster import solid, InputDefinition, OutputDefinition, String
 @solid(input_defs=[InputDefinition(name="bucket", dagster_type=String), InputDefinition(name="data_path",dagster_type=String)],
        output_defs=[OutputDefinition(name="data_path", dagster_type=String)])
 
-def sync_data(context, bucket, data_path):
-    os.system(f"aws s3 sync s3://{bucket} {data_path} > /project/etl/s3-logs")
+
+
+
+def sync_data(context, bucket, origins, data_path):
+    for origin in origins:
+        os.system(f"aws s3 sync s3://{bucket}/{origin} {data_path}/{origin} > /project/etl/s3-logs")
     return data_path
